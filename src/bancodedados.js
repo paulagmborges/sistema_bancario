d95d0a6ee92d82088a5cb364422a4853c0ae897e
paula borges <paulabzambeli@gmail.com>
2023-12-10 09:03:02 -0300

    banco de dados

---
+++ src/bancodedados.js
@@ -0,0 +1,16 @@
+module.exports = {
+    banco: {
+        nome: 'Cubos Bank',
+        numero: '123',
+        agencia: '0001',
+        senha: 'Cubos123Bank'
+    },
+    identificadorConta: 1,
+    contas: [
+         
+
+    ],
+    saques: [],
+    depositos: [],
+    transferencias: []
+}
